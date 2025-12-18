db7c11d8ddce8259415d4965a55ce50b108c8b32
Maksim <alesinmaks007@gmail.com>
2025-12-18 21:30:32 +0300

    Добавьте файлы проекта.

---
+++ practicewinter2/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace practicewinter2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 437);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Счет: 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 413);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Время: 60";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(260, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 478);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Захват объектов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ practicewinter2/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace practicewinter2
+{
+    public partial class Form1 : Form
+    {
+        class Player
+        {
+            public Rectangle Bounds { get; set; }
+            public int Speed { get; set; } = 5;
+
+            public Player(Point location)
+            {
+                Bounds = new Rectangle(location, new Size(50, 50));
+            }
+
+            public void Move(int deltaX, int deltaY, Panel gamePanel)
+            {
+                int newX = Bounds.X + deltaX;
+                int newY = Bounds.Y + deltaY;
+
+                if (newX >= 0 && newX + Bounds.Width <= gamePanel.Width)
+                    Bounds = new Rectangle(newX, Bounds.Y, Bounds.Width, Bounds.Height);
+
+                if (newY >= 0 && newY + Bounds.Height <= gamePanel.Height)
+                    Bounds = new Rectangle(Bounds.X, newY, Bounds.Width, Bounds.Height);
+            }
+        }
+
+        class Collectible
+        {
+            public Rectangle Bounds { get; set; }
+            public bool IsCollected { get; set; }
+
+            public Collectible(Random random, Panel gamePanel)
+            {
+                int x = random.Next(0, gamePanel.Width - 15);
+                int y = random.Next(0, gamePanel.Height - 15);
+                Bounds = new Rectangle(x, y, 15, 15);
+            }
+        }
+
+        private Player player;
+        private List<Collectible> collectibles;
+        private Random random;
+        private int score;
+        private int gameTime;
+        private bool isGameActive;
+        private DateTime lastSpawnTime;
+        private int spawnInterval = 2000;
+        private System.Collections.Hashtable keyTable = new System.Collections.Hashtable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            random = new Random();
+            collectibles = new List<Collectible>();
+            score = 0;
+            gameTime = 60;
+            isGameActive = false;
+            spawnInterval = 2000;
+
+            player = new Player(new Point(
+                panel1.Width / 2 - 10,
+                panel1.Height / 2 - 10
+            ));
+
+            UpdateLabels();
+            panel1.Paint += Panel1_Paint;
+
+        }
+
+        private void UpdateLabels()
+        {
+            label1.Text = "Счет: " + score;
+            label2.Text = "Время: " + gameTime;
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(panel1.BackColor);
+
+            using (Brush playerBrush = new SolidBrush(Color.LightSeaGreen))
+            {
+                e.Graphics.FillRectangle(playerBrush, player.Bounds);
+                e.Graphics.DrawRectangle(Pens.Black, player.Bounds);
+            }
+
+            foreach (var obj in collectibles)
+            {
+                if (!obj.IsCollected)
+                {
+                    using (Brush objBrush = new SolidBrush(Color.Magenta))
+                    {
+                        e.Graphics.FillEllipse(objBrush, obj.Bounds);
+                        e.Graphics.DrawEllipse(Pens.Black, obj.Bounds);
+                    }
+                }
+            }
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (isGameActive)
+            {
+                if (keyData == Keys.Left || keyData == Keys.Right ||
+                    keyData == Keys.Up || keyData == Keys.Down)
+                {
+                    keyTable[keyData] = true;
+                    return true; 
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            if (keyTable.ContainsKey(e.KeyCode))
+            {
+                keyTable.Remove(e.KeyCode);
+            }
+        }
+
+        private void StartGame()
+        {
+            collectibles.Clear();
+            score = 0;
+            gameTime = 60;
+            spawnInterval = 2000;
+            isGameActive = true;
+
+            keyTable.Clear();
+
+            player.Bounds = new Rectangle(
+                panel1.Width / 2 - 10,
+                panel1.Height / 2 - 10,
+                50, 50
+            );
+
+            for (int i = 0; i < 3; i++)
+            {
+                collectibles.Add(new Collectible(random, panel1));
+            }
+
+            lastSpawnTime = DateTime.Now;
+
+            timer1.Start();
+            timer2.Start();
+            UpdateLabels();
+            this.Focus();
+            panel1.Invalidate();
+        }
+
+        private void EndGame()
+        {
+            isGameActive = false;
+            timer1.Stop();
+            timer2.Stop();
+            keyTable.Clear();
+
+            MessageBox.Show("Игра окончена! Ваш счет: " + score,
+                "Результат",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!isGameActive) return;
+
+            if (keyTable.ContainsKey(Keys.Left))
+                player.Move(-player.Speed, 0, panel1);
+            if (keyTable.ContainsKey(Keys.Right))
+                player.Move(player.Speed, 0, panel1);
+            if (keyTable.ContainsKey(Keys.Up))
+                player.Move(0, -player.Speed, panel1);
+            if (keyTable.ContainsKey(Keys.Down))
+                player.Move(0, player.Speed, panel1);
+
+            for (int i = collectibles.Count - 1; i >= 0; i--)
+            {
+                if (player.Bounds.IntersectsWith(collectibles[i].Bounds))
+                {
+                    collectibles.RemoveAt(i);
+                    score++;
+                    UpdateLabels();
+                }
+            }
+
+            if ((DateTime.Now - lastSpawnTime).TotalMilliseconds > spawnInterval)
+            {
+                collectibles.Add(new Collectible(random, panel1));
+                lastSpawnTime = DateTime.Now;
+
+                if (spawnInterval > 500)
+                    spawnInterval -= 50;
+            }
+
+            panel1.Invalidate();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (!isGameActive) return;
+
+            gameTime--;
+            UpdateLabels();
+
+            if (gameTime <= 0)
+            {
+                EndGame();
+            }
+        }
+    }
+}
